5934336ae8c69009ed77583e7d2730b4e96e3de0
Jordo754 <jdm3732@GDD-10.main.ad.rit.edu>
2015-11-06 23:29:09 -0500

    Unity is Dumb
    Overall failure of trying to add resources and their properties.

--- Unity/HumanResourcesGame/Assets/Code/Constants.cs
+++ Unity/HumanResourcesGame/Assets/Code/Constants.cs
@@ -1,18 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Constants : MonoBehaviour {
+	//needed arrays/lists
+	private static List<GameObject[]> doodadsList;
+	private static List<Material> texturesList;
 
     //Building sizes
     public const int BOX_HAB = 0;
     public const int MED_HAB = 1;
     public const int LARGE_HAB = 2;
 
-	void Start () {
+	//Resources:
+	//common
+	public const int SAND = 0;
+	public const int WOOD = 1;
+	public const int GOLD = 2;
 	
+	//rare
+	public const int FRANCIUM = 3;
+	
+	//exotic
+	public const int TEARS = 4;
+
+	//Textures:
+	//common
+	public Material sandMaterial;
+	public Material woodMaterial;
+	public Material goldMaterial;
+
+	//rare
+	public Material franciumMaterial;
+
+	//exotic
+	public Material tearsMaterial;
+
+	//Doodads:
+	//common
+	public GameObject[] sandDoodads;
+	public GameObject[] woodDoodads;
+	public GameObject[] goldDoodads;
+
+	//rare
+	public GameObject[] franciumDoodads;
+
+	//exotic
+	public GameObject[] tearsDoodads;
+
+	void Awake(){
+		//populate
+		texturesList = new List<Material>{sandMaterial, woodMaterial, goldMaterial, franciumMaterial, tearsMaterial};
+		doodadsList = new List<GameObject[]>{sandDoodads, woodDoodads, goldDoodads, franciumDoodads, tearsDoodads};
+	}
+
+	void Start () {
+		
 	}
 	
 	void Update () {
 	
+	}
+
+	//takes resource Id number and returns coressponing texture and doodads[]
+	public ArrayList GetResourceInfo(int resourceId) {
+		ArrayList resourceInfo = new ArrayList ();
+
+		resourceInfo.Add(texturesList [resourceId]);
+		resourceInfo.Add(doodadsList [resourceId]);
+
+		return resourceInfo;
 	}
 }
--- Unity/HumanResourcesGame/Assets/Code/Creature.cs
+++ Unity/HumanResourcesGame/Assets/Code/Creature.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Creature : MonoBehaviour {
 
-	// Use this for initialization
+	//resource attrtibutes
+	List<GameObject> resourceDoodads;
+	public Material currentMat;
+	private int testCount = 0;
+
 	void Start () {
-	
+		resourceDoodads = new List<GameObject>();
+		ApplyResource (0);
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+
 	}
+
+	//get a resource's info based on an Id
+	void ApplyResource(int resourceId){
+		/*ArrayList info = GameObject.FindGameObjectWithTag("GameManager").GetComponent<Constants>().GetResourceInfo (resourceId);
+
+		currentMat = info [0] as Material;
+		//print (currentMat.name);
+
+		GameObject[] parse = info [1] as GameObject[];
+
+		foreach (GameObject piece in parse) {
+			resourceDoodads.Add (piece);
+		}
+
+		//Apply texture 
+		Renderer render = GetComponent<Renderer> ();
+		render.material = currentMat;*/
+	}
+
 }
